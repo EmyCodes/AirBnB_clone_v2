f365cac003cfdc52ccbfb047d7b50496c1c0f2ed
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-05-09 15:30:03 +0000

    city

--- models/city.py
+++ models/city.py
@@ -2,9 +2,13 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    # a table that creates a relationship
+    City = relationship('Place', cascade='all, delete', backref='cities') 
